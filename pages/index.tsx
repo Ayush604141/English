4facbb9ddf0bfc39a05bcaeef24c0abb97744936
Ayush Shrivastav <ayush41red@gmail.com>
2025-08-16 21:24:03 +0530

    fix: update vocab import path and add type assertion for VocabList

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,12 +1,13 @@
 import React from "react";
 import VocabList from "../components/VocabList";
-import Vocab from "@/words.json";
+import Vocab from "../words.json";
 import { Stack } from "@mui/material";
+import { VocabItem } from "../utils/common";
 
 export default function Index() {
   return (
     <Stack>
-      <VocabList data={Vocab} />
+      <VocabList data={Vocab as VocabItem[]} />
     </Stack>
   );
 }
